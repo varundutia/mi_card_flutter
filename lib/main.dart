b393500988d39b440b325aa8dad88981b638754a
varundutia <51284086+varundutia@users.noreply.github.com>
2020-12-05 23:09:22 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,11 +2,87 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    MyApp()
+  );
+}
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              CircleAvatar(
+                backgroundImage: AssetImage('images/varunpic.jpg'),
+                radius: 50,
+              ),
+              Text('Varun Dutia',
+              style: TextStyle(
+                fontFamily: 'JetBrains Mono',
+                fontSize: 40.5,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+              ),
+              Text('DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                fontSize: 20,
+                color: Colors.teal.shade100,
+              ),
+              ),
+              SizedBox(
+                height: 20.0,
+                width: 150.0,
+                child: Divider(
+                  thickness: 1.0,
+                  color: Colors.teal.shade100,
+                ),
+              ),
+               Card(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal.shade500,
+                  ),
+                  title: Text(
+                    '+91 8220618104',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                      color: Colors.teal.shade500,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0,horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal.shade500,
+                  ),
+                  title: Text(
+                      'varundutia.h@gmail.com',
+                      style: TextStyle(
+                        fontFamily: 'Source Sans Pro',
+                        fontSize: 20,
+                        color: Colors.teal.shade500,
+                      ),
+                ),
+              ),
+            ),
+        ],
+        ),
       ),
-    ),
-  );
+      ),
+    );
+  }
+
 }
